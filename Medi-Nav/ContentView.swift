2b37dae5a8a0479870309ad6e8d711086f23bc16
DilshaSM <Luckyme@98>
2026-03-11 16:45:03 +0530

    developed bottom tab bar navigation

--- Medi-Nav/ContentView.swift
+++ Medi-Nav/ContentView.swift
@@ -1,35 +1,50 @@
 
+
 import SwiftUI
 
 struct ContentView: View {
-
     @State private var currentTab: AppTab = .home
     
     var body: some View {
         ZStack(alignment: .bottom) {
             
-          
+           
             TabView(selection: $currentTab) {
-                HomeView()
-                    .tag(AppTab.home)
-                    .toolbar(.hidden, for: .tabBar)
                 
                
-                Text("Booking Feature")
-                    .tag(AppTab.booking)
-                    .toolbar(.hidden, for: .tabBar)
+                NavigationStack {
+                    HomeView()
+                }
+                .tag(AppTab.home)
+                .toolbar(.hidden, for: .tabBar)
                 
-                Text("Chat Feature")
-                    .tag(AppTab.chat)
-                    .toolbar(.hidden, for: .tabBar)
+               
+                NavigationStack {
+                    FindDoctorView()
+                }
+                .tag(AppTab.booking)
+                .toolbar(.hidden, for: .tabBar)
                 
-                Text("Account Settings")
-                    .tag(AppTab.account)
-                    .toolbar(.hidden, for: .tabBar)
+              
+                NavigationStack {
+                    ChatView()
+                }
+                .tag(AppTab.chat)
+                .toolbar(.hidden, for: .tabBar)
+                
+               
+                NavigationStack {
+                    AccountView()
+                }
+                .tag(AppTab.account)
+                .toolbar(.hidden, for: .tabBar)
             }
             
+          
             CustomTabBar(selectedTab: $currentTab)
         }
+        
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
